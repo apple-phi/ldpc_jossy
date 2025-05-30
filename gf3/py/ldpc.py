1a3f8dda3aa173c6020bad82ae645862a4c54c8d
apple-phi <lkn849@gmail.com>
2025-05-30 19:26:47 +0100

    Fix linking & type error

--- gf3/py/ldpc.py
+++ gf3/py/ldpc.py
@@ -1,6 +1,8 @@
 import numpy as np
 import ctypes as ct
 
+from .link import c_ldpc
+
 class code:
     def __init__(self, standard = '802.11n', rate = '1/2', z=27, ptype='A'):
         self.standard = standard
@@ -392,6 +394,7 @@ class code:
                 vflag[yi] = 1
                 intrlv[xi] = yi
         intrlv = np.argsort(intrlv)
+        intrlv = intrlv.astype(np.int32)
         return vdeg, cdeg, intrlv
 
 
@@ -463,7 +466,7 @@ class code:
         vdeg = self.vdeg
         cdeg = self.cdeg
         intrlv = self.intrlv
-        c_ldpc = ct.CDLL('./bin/c_ldpc.so')
+        
         # preliminary consistency checks
         if len(ch) != len(vdeg):
             raise NameError('Channel inputs not consistent with variable degrees')
@@ -489,12 +492,10 @@ class code:
         return app, it
 
     def Lxor(self, L1, L2, corrflag=1):
-        c_ldpc = ct.CDLL('./bin/c_ldpc.so')
         c_ldpc.Lxor.restype = ct.c_double
         return c_ldpc.Lxor(ct.c_double(L1), ct.c_double(L2), corrflag)
 
     def Lxfb(self, L, corrflag=1):
-        c_ldpc = ct.CDLL('./bin/c_ldpc.so')
         dc = len(L)
         L = np.array(L, dtype=float)
         L_p = L.ctypes.data_as(ct.POINTER(ct.c_double))
